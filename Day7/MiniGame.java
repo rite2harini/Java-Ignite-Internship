badc605edd25b061fe17b7041f8eec27b60040c1
IpshitaDutta <cse.25bcsa54@silicon.ac.in>
2026-06-03 17:29:19 +0530

    Update MiniGame.java

--- Day7/MiniGame.java
+++ Day7/MiniGame.java
@@ -1,25 +1,61 @@
-/*
-ADVANCED 2 – Mini Game Character System
+package com.sevenprog;
+import java.util.Scanner;
+public class GameCharacter {
+private String name;
+private int health;
+private int attackPower;
+public GameCharacter(String name,int health,int attackPower)
+{
+	this.name=name;
+	this.health=health;
+	this.attackPower=attackPower;
+}
+public void attack(GameCharacter enemy)
+{
+	enemy.health=enemy.health-this.attackPower;
+	if(enemy.health<0)
+		enemy.health=0;
+	System.out.println(this.name+" attacked "+enemy.name+" and reduced health by"+this.attackPower);
+}
+public void heal()
+{
+health=health+20;
+System.out.println(name+"healed and gained 20 health");
 
-Create a class GameCharacter:
+}
+public void showStats()
+{
+	System.out.println("Name:"+name+"\n"+"Health"+health+"\n"+"Attack Power:"+attackPower+"\n");
+	System.out.println();
+}
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter name,health status and attacking power status respectively of 1st player\n");
+				String n;int a,b;
+				n=sc.nextLine();
+				
+				a=sc.nextInt();
+				b=sc.nextInt();
+				sc.nextLine();
+				System.out.println("Enter name,health status and attacking power status respectively of 2nd player");
+				String m;int x,y;
+				m=sc.nextLine();
+				
+				x=sc.nextInt();
+				y=sc.nextInt();
+				GameCharacter player1=new GameCharacter(n,a,b);
+				GameCharacter player2=new GameCharacter(m,x,y);
+				player1.showStats();
+				player2.showStats();
+				player1.attack(player2);
+				player2.heal();
+				System.out.println("\nAfter actions:\n");
+				player1.showStats();
+				player2.showStats();
+				
+				
+				
 
-Attributes:
-name
-health (PRIVATE)
-attackPower (PRIVATE)
-Requirements:
-Constructor using this
-Proper encapsulation
-Methods:
-attack(GameCharacter enemy)
-heal()
-showStats()
-Rules:
-attack reduces enemy health
-heal increases own health
-health should never be accessed directly
-Twist:
+	}
+}
 
-Each attack should show:
-*/
-X attacked Y and reduced health by Z
