80e72ce75b25d6437098b0c54e8b646a711bb4fa
SHAKTIPRASADDASH-25BCSD92 <dashshakti07@gmail.com>
2026-06-04 03:36:53 +0530

    Update Classroom_Result_System.java

--- Day8/Classroom_Result_System.java
+++ Day8/Classroom_Result_System.java
@@ -11,3 +11,38 @@ Output Example
 Passed Students: 7
 Failed Students: 3
 */
+package java_internship_assignment;
+import java.util.Scanner;
+
+public class classroomresultsystem {
+	public static void main(String[] args) {
+								
+		Scanner sc=new Scanner(System.in);
+				int i,p=0,f=0;
+				int[]students=new int[10];
+				for(i=0;i<10;i++)
+				{	
+					System.out.printf("Enter the mark of the %d student ",i+1);
+					students[i]=sc.nextInt();
+				}
+				
+				for(i=0;i<10;i++)
+				{	
+					if(students[i]>=35)
+					{	
+						p++;
+						
+					}
+					else if(students[i]<35)
+					{	
+						f++;
+					}	
+				}
+				
+					
+					System.out.println("Pass Students:"+p);
+					System.out.println("Fail Students:"+f);
+
+				}
+
+}
