547b19ff469b329d7186e23b8e01cdbe5b9da3ad
SHAKTIPRASADDASH-25BCSD92 <dashshakti07@gmail.com>
2026-06-04 02:05:45 +0530

    Update StudentMarksDisplay.java

--- Day8/StudentMarksDisplay.java
+++ Day8/StudentMarksDisplay.java
@@ -13,3 +13,30 @@ Student Marks:
 82
 55
   */
+package java_internship_assignment;
+import java.util.Scanner;
+
+public class Studentsmarksdisplay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int i;
+		int[]arr=new int[5];
+		for(i=0;i<5;i++)
+		{	
+			System.out.println("Enter the marks of 5 student ");
+			arr[i]=sc.nextInt();
+		}
+		for(i=0;i<5;i++)
+		{	
+
+			System.out.println("Students marks:");
+			System.out.println(arr[i]);
+		}
+				
+
+	}
+
+}
+
