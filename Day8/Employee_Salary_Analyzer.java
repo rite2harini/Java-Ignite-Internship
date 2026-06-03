3b44edf67e1aa310a4baa9ad80b5d5d61ee7dc6f
ece25becd86-NANDINIBISWAPRIYA <ece.25becd86@silicon.ac.in>
2026-06-03 21:22:02 +0530

    Update Employee_Salary_Analyzer.java 25BECD86_Nandini Biswapriya

--- Day8/Employee_Salary_Analyzer.java
+++ Day8/Employee_Salary_Analyzer.java
@@ -11,3 +11,34 @@ Hint
 
 Use a loop and a sum variable.
   */
+
+package JavaIgniteDay8;
+import java.util.Scanner;
+
+public class Employee_Salary_Analyzer {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Scanner sc = new Scanner(System.in);
+			int[] arr = new int[5];
+			System.out.println("Enter salaries of 5 employees:");
+			for(int i =0;i<arr.length;i++)
+			{
+				arr[i]= sc.nextInt();
+			}
+			
+			int sum=0;
+			for(int i=0;i<arr.length;i++)
+			{
+				sum+=arr[i];
+			}
+			
+			System.out.println("Total salary: Rs."+sum);
+			System.out.println("Average salary: Rs."+(sum/5));
+			
+			sc.close();
+		}
+
+	}
+
+
