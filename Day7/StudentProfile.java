704324c5601df73be758e7756e14b48f900b03f1
IpshitaDutta <cse.25bcsa54@silicon.ac.in>
2026-06-03 17:32:20 +0530

    Update StudentProfile.java

--- Day7/StudentProfile.java
+++ Day7/StudentProfile.java
@@ -1,19 +1,42 @@
-/*
-(EASY)
-Create a class Student:
+package com.sevenprog;
+import java.util.Scanner;
+class Mobile {
+    String brand;
+    private double price;
+    int storage;
 
-Requirements:
-name
-age
-marks
-Tasks:
-Initialize using constructor
-Create 2 student objects
-Display student details using method
-Twist:
+    public Mobile(String brand, double price, int storage) {
+        this.brand = brand;
+        this.price = price;
+        this.storage = storage;
+    }
 
-Marks should not be accessed directly.
+    public void displayDetails() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Price: " + price);
+        System.out.println("Storage: " + storage + " GB");
+        System.out.println();
+    }
 
-Write your code below
-----------------------------------------
-*/
+
+
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter brand,price and storage of 1st mobile respectively");
+      String a;int b,c;
+      a=sc.next();
+      b=sc.nextInt();
+      c=sc.nextInt();
+        Mobile m1 = new Mobile(a,b,c);
+        System.out.println("Enter brand,price and storage of 2nd mobile respectively");
+        String m;int x,y;
+        m=sc.next();
+        x=sc.nextInt();
+        y=sc.nextInt();
+        Mobile m2 = new Mobile(m,x,y);
+
+        m1.displayDetails();
+        m2.displayDetails();
+    }
+
+}
