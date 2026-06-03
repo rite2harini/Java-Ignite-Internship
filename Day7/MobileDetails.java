ce4705bbbda643cada16d610bff90cc8eb5470d2
IpshitaDutta <cse.25bcsa54@silicon.ac.in>
2026-06-03 17:30:48 +0530

    Update MobileDetails.java

--- Day7/MobileDetails.java
+++ Day7/MobileDetails.java
@@ -1,18 +1,42 @@
-/*
-EASY
-Mobile Details System
+package com.sevenprog;
+import java.util.Scanner;
+class Mobile {
+    String brand;
+    private double price;
+    int storage;
 
-Create a class Mobile:
+    public Mobile(String brand, double price, int storage) {
+        this.brand = brand;
+        this.price = price;
+        this.storage = storage;
+    }
 
-Attributes:
-brand
-price
-storage
-Tasks:
-Use constructor to initialize values
-Create 2 mobile objects
-Display details using method
-Twist:
+    public void displayDetails() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Price: " + price);
+        System.out.println("Storage: " + storage + " GB");
+        System.out.println();
+    }
 
-Price must be private and only shown via method.
-  */
+
+
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter brand,price and storage of 1st mobile respectively");
+      String a;int b,c;
+      a=sc.next();
+      b=sc.nextInt();
+      c=sc.nextInt();
+        Mobile m1 = new Mobile(a,b,c);
+        System.out.println("Enter brand,price and storage of 2nd mobile respectively");
+        String m;int x,y;
+        m=sc.next();
+        x=sc.nextInt();
+        y=sc.nextInt();
+        Mobile m2 = new Mobile(m,x,y);
+
+        m1.displayDetails();
+        m2.displayDetails();
+    }
+
+}
