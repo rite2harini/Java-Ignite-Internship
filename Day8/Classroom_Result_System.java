f63f97a2f73147b0be0dc217f5a0b64403adc795
Amankumar0270 <cse.25bcsi39@silicon.ac.in>
2026-06-04 10:24:28 +0530

    Update Classroom_Result_System.java

--- Day8/Classroom_Result_System.java
+++ Day8/Classroom_Result_System.java
@@ -11,3 +11,25 @@ Output Example
 Passed Students: 7
 Failed Students: 3
 */
+public class ClassroomResultSystem {
+
+    public static void main(String[] args) {
+
+        int[] marks = {78, 45, 22, 90, 67, 30, 55, 81, 34, 40};
+
+        int passed = 0;
+        int failed = 0;
+
+        for (int i = 0; i < 10; i++) {
+
+            if (marks[i] >= 35) {
+                passed++;
+            } else {
+                failed++;
+            }
+        }
+
+        System.out.println("Passed Students: " + passed);
+        System.out.println("Failed Students: " + failed);
+    }
+}
