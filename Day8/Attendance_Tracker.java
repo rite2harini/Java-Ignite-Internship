37ac2ef96b1246334c3e1e62b57cdb3e24b55441
Amankumar0270 <cse.25bcsi39@silicon.ac.in>
2026-06-04 10:21:33 +0530

    Update Attendance_Tracker.java

--- Day8/Attendance_Tracker.java
+++ Day8/Attendance_Tracker.java
@@ -14,3 +14,28 @@ Total Present Students
 Total Absent Students
 Attendance Percentage
 */
+public class AttendanceTracker {
+
+    public static void main(String[] args) {
+
+        int[] attendance = {1, 0, 1, 1, 0, 1, 1, 1, 0, 1};
+
+        int present = 0;
+        int absent = 0;
+
+        for (int i = 0; i < 10; i++) {
+
+            if (attendance[i] == 1) {
+                present++;
+            } else {
+                absent++;
+            }
+        }
+
+        double percentage = (present * 100.0) / 10;
+
+        System.out.println("Total Present Students: " + present);
+        System.out.println("Total Absent Students: " + absent);
+        System.out.println("Attendance Percentage: " + percentage + "%");
+    }
+}
