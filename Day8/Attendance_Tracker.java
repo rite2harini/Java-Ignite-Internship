f13f877e2df9199592d16e02dde0b767efcd4b12
25BEEG04 <abhijitbiswal700@gmail.com>
2026-06-04 15:44:58 +0530

    Update Attendance_Tracker.java

--- Day8/Attendance_Tracker.java
+++ Day8/Attendance_Tracker.java
@@ -1,16 +1,29 @@
-/*
-1. Attendance Tracker (Medium)
+package JavaIgniteDay8;
+import java.util.Scanner;
+public class Attendance_tracker {
 
-A class has attendance records for 10 students.
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int [] att = new int [10];
+		int i,pt=0,abs=0,pt_percent;
+		System.out.println("Enter 1 for present and 0 for abesnt");
+		for(i=0;i<10;i++)
+			att[i]=sc.nextInt();
+		for(i=0;i<10;i++) {
+			if(att[i]  == 1) {
+				pt++;
+			}
+			if(att[i] == 0) {
+				abs++;
+			}
+		}
+		System.out.printf("Total no of students is 10\n");
+		System.out.printf("%d are present\n%d are absent\n",pt,abs);
+		pt_percent = (pt*100)/10;
+		System.out.println("Present percentage is "+ pt_percent);
 
-Store attendance as:
+		sc.close();
+	}
 
-1 = Present
-0 = Absent
-
-Calculate:
-
-Total Present Students
-Total Absent Students
-Attendance Percentage
-*/
+}
