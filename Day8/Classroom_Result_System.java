9c4af0d4300d15801a3b51f6ed0fd0c3acff7417
25BEEG04 <abhijitbiswal700@gmail.com>
2026-06-04 15:49:46 +0530

    Update Classroom_Result_System.java

--- Day8/Classroom_Result_System.java
+++ Day8/Classroom_Result_System.java
@@ -1,13 +1,24 @@
-/*
-Medium – Classroom Result System
+package JavaIgniteDay8;
+import java.util.Scanner;
+public class Class_mark {
 
-Create a program that:
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int marks[]=new int [10];
+		int i,pass=0,fail=0;
+		System.out.println("Enter the marks for the 10 students :");
+		for(i=0;i<10;i++) {
+			marks[i] = sc.nextInt();
+		}
+		for(i=0;i<10;i++) {
+			if(marks[i] >= 35)
+				pass++;
+			else
+				fail++;
+		}
+		System.out.printf("Number of students passed Students : %d\nNumber of studens failed Student : %d\n",pass,fail);
+		sc.close();
+	}
 
-Stores marks of 10 students in an array
-Count:
-Number of students who passed (marks >= 35)
-Number of students who failed (marks < 35)
-Output Example
-Passed Students: 7
-Failed Students: 3
-*/
+}
