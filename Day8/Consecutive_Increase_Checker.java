5ce102be5463bccf7dd12b05ba95fcd6b596cea6
debasisswain2007-glitch <debasis.swain2007@gmail.com>
2026-06-04 07:51:01 +0530

    Update Consecutive_Increase_Checker.java 25BEEC02_Debasis Swain

--- Day8/Consecutive_Increase_Checker.java
+++ Day8/Consecutive_Increase_Checker.java
@@ -20,3 +20,36 @@ Output:
 
 Array is NOT in increasing order
   */
+import java.util.Scanner;
+
+public class ConsecutiveIncreaseChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[5];
+        
+        // Input 5 numbers
+        System.out.println("Enter 5 numbers:");
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Number " + (i + 1) + ": ");
+            numbers[i] = sc.nextInt();
+        }
+        
+        // Check if strictly increasing
+        boolean isIncreasing = true;
+        for (int i = 0; i < 4; i++) {
+            if (numbers[i] >= numbers[i + 1]) {
+                isIncreasing = false;
+                break;
+            }
+        }
+        
+        // Display result
+        if (isIncreasing) {
+            System.out.println("Array is in increasing order");
+        } else {
+            System.out.println("Array is NOT in increasing order");
+        }
+        
+        sc.close();
+    }
+}
