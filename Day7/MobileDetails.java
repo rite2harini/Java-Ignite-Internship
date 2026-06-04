8f9c834324a0b327c252b6fb91dcb6e8386bb3d9
Akshat-Singh29 <akshatksingh29@gmail.com>
2026-06-05 00:45:59 +0530

    Update MobileDetails.java

--- Day7/MobileDetails.java
+++ Day7/MobileDetails.java
@@ -16,3 +16,28 @@ Twist:
 
 Price must be private and only shown via method.
   */
+public class Mobile {
+    private String brand;
+    private double price;
+    private int storage;
+
+    public Mobile(String brand, double price, int storage) {
+        this.brand = brand;
+        this.price = price;
+        this.storage = storage;
+    }
+
+    public void displayDetails() {
+        System.out.println(this.brand);
+        System.out.println(this.price);
+        System.out.println(this.storage);
+    }
+
+    public static void main(String[] args) {
+        Mobile mobile1 = new Mobile("Samsung", 899.99, 256);
+        Mobile mobile2 = new Mobile("Apple", 999.00, 512);
+
+        mobile1.displayDetails();
+        mobile2.displayDetails();
+    }
+}
