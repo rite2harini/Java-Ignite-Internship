737a54a78ce96902169d14c1728e3f2f96f6d722
Akshat-Singh29 <akshatksingh29@gmail.com>
2026-06-05 00:37:13 +0530

    Implement attendance tracking functionality

--- Day8/Attendance_Tracker.java
+++ Day8/Attendance_Tracker.java
@@ -14,3 +14,25 @@ Total Present Students
 Total Absent Students
 Attendance Percentage
 */
+public class AttendanceTracker {
+    public static void main(String[] args) {
+        int[] attendance = {1, 0, 1, 1, 1, 0, 1, 1, 0, 1};
+        int presentCount = 0;
+        int absentCount = 0;
+        int totalStudents = attendance.length;
+
+        for (int i = 0; i < totalStudents; i++) {
+            if (attendance[i] == 1) {
+                presentCount++;
+            } else if (attendance[i] == 0) {
+                absentCount++;
+            }
+        }
+
+        double percentage = ((double) presentCount / totalStudents) * 100;
+
+        System.out.println("Total Present Students: " + presentCount);
+        System.out.println("Total Absent Students: " + absentCount);
+        System.out.println("Attendance Percentage: " + percentage + "%");
+    }
+}
