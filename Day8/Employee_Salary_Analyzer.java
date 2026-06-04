382e7dc0ef326ea12c37d699b7bf3119dc372b0a
nehamnishesh7 <nisheshneham7@gmail.com>
2026-06-04 11:30:21 +0530

    Update Employee_Salary_Analyzer.java

--- Day8/Employee_Salary_Analyzer.java
+++ Day8/Employee_Salary_Analyzer.java
@@ -11,3 +11,18 @@ Hint
 
 Use a loop and a sum variable.
   */
+public class SalaryAnalyzer {
+    public static void main(String[] args) {
+        double[] salaries = {45000.0, 55000.0, 60000.0, 48000.0, 52000.0};
+        double totalSalary = 0;
+
+        for (int i = 0; i < salaries.length; i++) {
+            totalSalary += salaries[i];
+        }
+
+        double averageSalary = totalSalary / salaries.length;
+
+        System.out.println("Total Salary: " + totalSalary);
+        System.out.println("Average Salary: " + averageSalary);
+    }
+}
