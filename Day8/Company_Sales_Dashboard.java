7d14ec5ee7d1b4f57c54b51c68c7cd817e016848
nehamnishesh7 <nisheshneham7@gmail.com>
2026-06-04 11:27:24 +0530

    Update Company_Sales_Dashboard.java

--- Day8/Company_Sales_Dashboard.java
+++ Day8/Company_Sales_Dashboard.java
@@ -20,3 +20,36 @@ Hint
 
 Use nested loops.
 */
+public class CompanySales {
+    public static void main(String[] args) {
+        double[][] sales = {
+            {1500.0, 2000.0, 1800.5, 2200.0},
+            {3000.0, 3100.0, 2900.0, 3500.0},
+            {1200.0, 1150.0, 1300.5, 1400.0}
+        };
+
+        double overallSales = 0;
+        double maxSales = -1;
+        int bestBranch = -1;
+
+        for (int i = 0; i < sales.length; i++) {
+            double branchTotal = 0;
+            
+            for (int j = 0; j < sales[i].length; j++) {
+                branchTotal += sales[i][j];
+            }
+            
+            System.out.println("Branch " + (i + 1) + " Total Sales: " + branchTotal);
+            
+            overallSales += branchTotal;
+            
+            if (branchTotal > maxSales) {
+                maxSales = branchTotal;
+                bestBranch = i + 1;
+            }
+        }
+
+        System.out.println("Overall Company Sales: " + overallSales);
+        System.out.println("Branch with Highest Total Sales: Branch " + bestBranch);
+    }
+}
