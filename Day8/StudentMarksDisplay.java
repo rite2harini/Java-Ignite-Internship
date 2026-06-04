cd4bf649819f4001e294becb782c5a4349911a18
25BEEG04 <abhijitbiswal700@gmail.com>
2026-06-04 16:54:50 +0530

    Update StudentMarksDisplay.java

--- Day8/StudentMarksDisplay.java
+++ Day8/StudentMarksDisplay.java
@@ -1,15 +1,22 @@
-/*
-Easy 1 – Student Marks Display
 
-Create a program that:
+package JavaIgniteDay8;
+import java.util.Scanner;
+public class Student_Marks {
 
-Stores 5 student marks in a 1D array
-Prints all marks using a loop
-Sample Output
-Student Marks:
-78
-65
-90
-82
-55
-  */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int mark[]=new int [10];
+		int i;
+		System.out.println("Enter the marks of the 10 students :");
+		for(i=0;i<10;i++) {
+			mark[i] = sc.nextInt();
+		}
+		System.out.println("Detailed marks :");
+		for(i=0;i<10;i++) {
+			System.out.printf("%d\n",mark[i]);
+		}
+		sc.close();
+	}
+
+}
